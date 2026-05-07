78c92a6ba14c7aabc44223e122eb3c19a9de25da
Thando <Thando.jele1402@gmail.com>
2026-05-08 01:57:26 +0200

    practicing Variable

---
+++ Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[]args){
+
+        String programmingLanguage = "Java";
+        int value = 14;
+        boolean ans = true;
+
+        System.out.println("The programming language used is " + programmingLanguage);
+        System.out.println("The value of the integer is " + value);
+        System.out.println("The value of the boolean is " + ans);
+
+    }
+}
